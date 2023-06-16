91ca37aa1938ac7bd693fa386518bc602db01319
ph luna <ph.luna.vieira@gmail.com>
2023-06-16 13:58:42 -0300

    refactor: change mock strategy in throw error handle test

--- src/presentation/controllers/tests/signup.spec.ts
+++ src/presentation/controllers/tests/signup.spec.ts
@@ -84,7 +84,7 @@ describe('[SignUp Controller]', () => {
   it('Should return 500 if EmailValidator throws', () => {
     const { sut, emailValidatorStub } = makeSut()
 
-    jest.spyOn(emailValidatorStub, 'isValid').mockImplementation(() => {
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
       throw new Error()
     })
 
