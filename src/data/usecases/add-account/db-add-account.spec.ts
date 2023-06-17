2bd6e0c12f7155ff86a598e568197b61c068a7cf
ph luna <ph.luna.vieira@gmail.com>
2023-06-17 16:58:50 -0300

    test: ensure DBAddAccount throw if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -75,4 +75,15 @@ describe('[DBAddAccount usecases]', () => {
 
     expect(addSpy).toHaveBeenCalledWith({ ...accountDataSample, password: 'encrypted_password' })
   })
+
+  it('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => {
+      reject(new Error())
+    }))
+
+    const promise = sut.add(accountDataSample)
+
+    await expect(promise).rejects.toThrow()
+  })
 })
