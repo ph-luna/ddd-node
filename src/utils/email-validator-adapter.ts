8556be6c3f6d6856f24a5a11fdef1faa9dbee358
ph luna <ph.luna.vieira@gmail.com>
2023-06-17 14:41:09 -0300

    feat: ensure EmailValidatorAdapter returns true or false if validator returns true or false

---
+++ src/utils/email-validator-adapter.ts
@@ -0,0 +1,9 @@
+import validator from 'validator'
+
+import type { IEmailValidator } from '../presentation/protocols/email-validator'
+
+export class EmailValidatorAdapter implements IEmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
