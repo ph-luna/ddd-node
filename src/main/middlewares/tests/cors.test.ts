71ffdb4a4d518854248793cd65b1a9416960e0c5
ph luna <ph.luna.vieira@gmail.com>
2023-06-19 10:04:01 -0300

    refactor: move all middleware tests to a separated folder

--- src/main/middlewares/tests/cors.test.ts
+++ src/main/middlewares/tests/cors.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest'
 
-import app from '../config/app'
+import app from '../../config/app'
 
 describe('[CORS Middleware]', () => {
   it('Should enable CORS', async () => {
