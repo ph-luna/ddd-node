071ec109a1512a4e9db1c46d3a2d221ca4d3596c
ph luna <ph.luna.vieira@gmail.com>
2023-06-20 16:03:10 -0300

    test: ensure mongodb reconects if connection closed

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -17,7 +17,7 @@ describe('[Mongo Repository - Account]', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
 
     await accountCollection.deleteMany({})
   })
