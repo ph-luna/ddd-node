7e7f19bd3d52ef70885f2a62c3a89de9255e3520
ph luna <ph.luna.vieira@gmail.com>
2023-07-24 12:44:48 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,14 +1,21 @@
 import type { IController, IHttpRequest, IHttpResponse } from '../../presentation/protocols'
+import type { ILogErrorRepository } from '../../data/protocols/log-error-repository'
 
 export class LogControllerDecorator implements IController {
   private readonly controller: IController
+  private readonly logErrorRepository: ILogErrorRepository
 
-  constructor (controller: IController) {
+  constructor (controller: IController, logErrorRepository: ILogErrorRepository) {
     this.controller = controller
+    this.logErrorRepository = logErrorRepository
   }
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
+
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.log(httpResponse.body.stack)
+    }
 
     return httpResponse
   }
