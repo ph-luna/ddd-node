5b47aedb7325a9956740cbf6ce725fe7fc817fa6
ph luna <ph.luna.vieira@gmail.com>
2023-07-25 14:04:28 -0300

    test: ensure logMongoRepository save an error log on mongodb

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,31 @@
+import type { Collection } from 'mongodb'
+
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('[Log Mongo Repository]', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+
+    await errorCollection.deleteMany({})
+  })
+
+  it('Should save an error log', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any error 404')
+
+    const count = await errorCollection.countDocuments()
+
+    expect(count).toBe(1)
+  })
+})
