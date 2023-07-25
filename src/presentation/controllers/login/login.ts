f45916091ba4928fd6add8a3618953d6fefe26dc
ph luna <ph.luna.vieira@gmail.com>
2023-07-25 15:55:24 -0300

    feat: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,10 +1,18 @@
 import type { IController, IHttpRequest, IHttpResponse } from '../../protocols'
 
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, success } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
 
 export class LoginController implements IController {
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'))
+    }
+
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'))
+    }
+
+    return success({})
   }
 }
