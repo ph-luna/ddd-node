3c2fe1bfc50014fe76668554c5e482d3813da01f
ph luna <ph.luna.vieira@gmail.com>
2023-08-09 16:17:34 -0300

    test: ensure SignUpController returns 400 if validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -152,4 +152,11 @@ describe('[SignUp Controller]', () => {
 
     expect(validateSpy).toBeCalledWith(accountSample)
   })
+
+  it('Should return 400 if validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest(accountSample))
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
