27e4d60b92f42276e75b56ccfb5904c6038200d4
ph luna <ph.luna.vieira@gmail.com>
2023-08-11 14:50:03 -0300

    test: ensure ValidationComposite returns the first error if more then one validation fails

--- src/presentation/helpers/validators/tests/validation-composite.spec.ts
+++ src/presentation/helpers/validators/tests/validation-composite.spec.ts
@@ -10,23 +10,31 @@ class ValidationStub implements IValidation {
 
 interface SutTypes {
   sut: ValidationComposite
-  validationStub: ValidationStub
+  validationStubs: ValidationStub[]
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = new ValidationStub()
-  const sut = new ValidationComposite([validationStub])
+  const validationStubs = [new ValidationStub(), new ValidationStub()]
+  const sut = new ValidationComposite(validationStubs)
   return {
     sut,
-    validationStub
+    validationStubs
   }
 }
 
 describe('[Validation Composite]', () => {
   it('Should return an error if any validation fails', () => {
-    const { sut, validationStub } = makeSut()
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new Error())
     const error = sut.validate({ field: 'any value' })
     expect(error).toEqual(new Error())
+  })
+
+  it('Should return the first error if more then one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error('first error'))
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new Error('second error'))
+    const error = sut.validate({ field: 'any value' })
+    expect(error).toEqual(new Error('first error'))
   })
 })
