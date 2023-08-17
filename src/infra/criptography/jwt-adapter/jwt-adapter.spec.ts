f2b509ba1acfc8caf7a0608093f9c1f94130000d
ph luna <ph.luna.vieira@gmail.com>
2023-08-17 15:24:29 -0300

    test: ensure JWTAdapter returns a token when sign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -11,11 +11,23 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 
+jest.mock('jsonwebtoken', () => ({
+  sign: (): string => {
+    return 'mock_access_token'
+  }
+}))
+
 describe('[JWT Adapter]', () => {
   it('Should call sign with corrects values', async () => {
     const { sut } = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toBeCalledWith({ id: 'any_id' }, 'mock_secret')
+  })
+
+  it('Should return a token on sign success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('mock_access_token')
   })
 })
