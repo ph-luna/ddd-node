120e40f76785c1ed4bd478bea79020c661fe1b6e
ph luna <ph.luna.vieira@gmail.com>
2023-08-17 15:14:53 -0300

    refactor: create sut factory in JWTAdapter tests

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,11 +2,20 @@ import jwt from 'jsonwebtoken'
 
 import { JwtAdapter } from './jwt-adapter'
 
+interface SutTypes {
+  sut: JwtAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new JwtAdapter('mock_secret')
+  return { sut }
+}
+
 describe('[JWT Adapter]', () => {
   it('Should call sign with corrects values', async () => {
-    const sut = new JwtAdapter('secret')
+    const { sut } = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
-    expect(signSpy).toBeCalledWith({ id: 'any_id' }, 'secret')
+    expect(signSpy).toBeCalledWith({ id: 'any_id' }, 'mock_secret')
   })
 })
