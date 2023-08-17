d3666d769653c53145b5a8fc822eddd46eb0bdf6
ph luna <ph.luna.vieira@gmail.com>
2023-08-17 15:11:40 -0300

    feat: ensure JWTAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+
+import type { IEncrypter } from '../../../data/protocols/criptography/encrypter'
+
+export class JwtAdapter implements IEncrypter {
+  private readonly secret: string
+
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  async encrypt (value: string): Promise<string> {
+    jwt.sign({ id: value }, this.secret)
+    return ''
+  }
+}
