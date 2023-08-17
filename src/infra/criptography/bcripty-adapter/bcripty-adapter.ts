ec50829bae8fef498a54f237d555a1042bfa61ac
ph luna <ph.luna.vieira@gmail.com>
2023-08-17 13:44:42 -0300

    feat: ensure BcryptAdapter returns false when compare fails

--- src/infra/criptography/bcripty-adapter/bcripty-adapter.ts
+++ src/infra/criptography/bcripty-adapter/bcripty-adapter.ts
@@ -16,7 +16,6 @@ export class BcryptAdapter implements IHasher, IHashComparer {
   }
 
   async compare (value: string, hash: string): Promise<boolean> {
-    await bcrypt.compare(value, hash)
-    return await new Promise(resolve => { resolve(true) })
+    return await bcrypt.compare(value, hash)
   }
 }
